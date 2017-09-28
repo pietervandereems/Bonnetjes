751a34800b85ff5686aba1d5d025ed3e1951baff
Pieter van der Eems <github@eemco.nl>
2017-09-28 12:38:06 +0200

    Switch from index.js to main.js as the to be executed main js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
